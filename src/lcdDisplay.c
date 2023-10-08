76a91a593a04f1fafdb869b2d3b39e1e3a6abcdd
Pablo Jean Rozario <pablo.jean.eel@gmail.com>
2023-10-08 18:13:25 -0300

    Fixed catastrofic problem into Enable pin Logic.

--- src/lcdDisplay.c
+++ src/lcdDisplay.c
@@ -21,7 +21,7 @@ void send_to_lcd (lcd_t *lcd, char data, int rs)
 		// first is sent the Upper 4 bits (higher nibble) over the interface
 		// in the second step, is sent the lower nibble
 		steps = 2;
-		_dataSend[0] = (data >> 4);
+		_dataSend[0] = ((data >> 4) & 0xF);
 		_dataSend[1] = (data & 0xF);
 		for (i=0 ; i<steps ; i++){
 			/* write the data to the respective pin */
@@ -32,10 +32,10 @@ void send_to_lcd (lcd_t *lcd, char data, int rs)
 
 			/* Toggle EN PIN to send the data
 			 */
+			_platform_delay_us(25);
 			_platform_gpio_write(lcd->gpios[LCD_E], 1);
-			_platform_delay_us(20);
+			_platform_delay_us(1);
 			_platform_gpio_write(lcd->gpios[LCD_E], 0);
-			_platform_delay_us(20);
 		}
 	}
 	else{
@@ -52,10 +52,10 @@ void send_to_lcd (lcd_t *lcd, char data, int rs)
 
 		/* Toggle EN PIN to send the data
 		 */
+		_platform_delay_us(25);
 		_platform_gpio_write(lcd->gpios[LCD_E], 1);
-		_platform_delay_us(20);
+		_platform_delay_us(1);
 		_platform_gpio_write(lcd->gpios[LCD_E], 0);
-		_platform_delay_us(20);
 	}
 }
 
@@ -73,13 +73,13 @@ void lcd_init (lcd_t *lcd)
 	else{
 		dispFont = DISPLAY_FONT_5x10;
 	}
-	_platform_delay_ms(50);  	// wait for >40ms
+	_platform_delay_ms(40);  	// wait for >40ms
 	lcd_cmd (lcd, LCD_CMD_FUNCTION_SET | DISPLAY_DL_8BIT);
 	_platform_delay_ms(5);  	// wait for >4.1ms
 	lcd_cmd (lcd, LCD_CMD_FUNCTION_SET | DISPLAY_DL_8BIT);
-	_platform_delay_us(200);  	// wait for >100us
+	_platform_delay_us(100);  	// wait for >100us
 	lcd_cmd (lcd, LCD_CMD_FUNCTION_SET | DISPLAY_DL_8BIT);
-	_platform_delay_us(200);	// wait for >100us
+	_platform_delay_ms(100);	// wait for >100us
 	if (lcd->interface == LCD_INTERFACE_4BIT){
 		// 4 bit initialisation
 		lcd_cmd (lcd, LCD_CMD_FUNCTION_SET | DISPLAY_DL_4BIT);  // 4bit mode
@@ -94,13 +94,13 @@ void lcd_init (lcd_t *lcd)
   // dislay initialisation
 	lcd->_cursor = DISPLAY_CONTROL_ON | DISPLAY_CURSOR_OFF | DISPLAY_BLINK_OFF;
 	lcd_cmd (lcd, LCD_CMD_FUNCTION_SET | dispDl | DISPLAY_N_2LINE | dispFont); // Function set --> DL=0 or 1, N = 1 (2 line display) F = 0 (5x8 characters)
-	_platform_delay_ms(1);
+	_platform_delay_ms(5);
 	lcd_cmd (lcd, LCD_CMD_DISP_CONTROL); //Display on/off control --> D=0,C=0, B=0  ---> display off
-	_platform_delay_ms(1);
+	_platform_delay_ms(5);
 	lcd_cmd (lcd, LCD_CMD_CLEAR);  // clear display
-	_platform_delay_ms(2);
+	_platform_delay_ms(5);
 	lcd_cmd (lcd, LCD_CMD_INC_CURSOR); //Entry mode set --> I/D = 1 (increment cursor) & S = 0 (no shift)
-	_platform_delay_ms(1);
+	_platform_delay_ms(5);
 	lcd_cmd (lcd, LCD_CMD_DISP_CONTROL | lcd->_cursor); //Display on/off control --> D = 1, C and B = 0. (Cursor and blink, last two bits)
 
 	lcd_cmd(lcd, LCD_CMD_RETURN_HOME);
@@ -222,8 +222,10 @@ void lcd_create_custom_char (lcd_t *lcd, lcd_custom_char_e custom, uint8_t *bitm
 	lcd_cmd(lcd, LCD_CMD_SET_CGRAM | offset);
 	for (i=0 ; i<rows ; i++){
 		lcd_data(lcd, bitmap[i]);
+		_platform_delay_ms(1);
 	}
 	lcd_cmd(lcd, LCD_CMD_RETURN_HOME);
+	_platform_delay_ms(1);
 }
 
 void lcd_backlight_set (lcd_t *lcd, uint8_t state){
